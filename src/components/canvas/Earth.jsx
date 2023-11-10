5cc76a8bfe742c85a5428cedbf533021511d6ebf
Jethro_Block <jethroblock22@gmail.COM>
2023-11-10 14:59:13 +0000

    set up earth canvas

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -1,4 +1,8 @@
-import React from 'react'
+import React, {Suspense} from 'react'
+import { Canvas } from '@react-three/fiber'
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei'
+import CanvasLoader from "../Loader"
+
 
 const Earth = () => {
   return (
@@ -6,4 +10,27 @@ const Earth = () => {
   )
 }
 
-export default Earth
+
+const EarthCanvas = () => {
+  return (
+    <Canvas
+    shadows
+    frameloop='demand'
+    gl={{preserveDrawingBuffer: true}}
+    camera={{}}
+    >
+    <Suspense fallback={<CanvasLoader />} >
+      <OrbitControls 
+      autoRotate
+      enableZoom={false}
+      maxPolarAngle={Math.PI / 2}
+      minPolarAngle={Math.PI / 2}
+
+      />
+      <Earth />
+    </Suspense>
+    </Canvas>
+  )
+}
+
+export default EarthCanvas
