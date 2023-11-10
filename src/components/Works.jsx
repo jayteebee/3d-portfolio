b8b3e18d7aec09f7c43df2fedffaceee573ff65e
Jethro_Block <jethroblock22@gmail.COM>
2023-11-10 12:13:33 +0000

    Works imports

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -1,5 +1,11 @@
 import React from 'react'
-import 
+import { Tilt } from 'react-parallax-tilt'
+import { motion } from 'framer-motion'
+import { styles } from '../styles'
+import { github } from '../assets'
+import { SectionWrapper } from '../hoc'
+import { projects } from '../constants'
+import { fadeIn, textVariant } from '../utils/motion'
 
 
 const Works = () => {
