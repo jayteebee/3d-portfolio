3d38bc85d1b2ecf531d54755f430de48a84732bd
Jethro_Block <jethroblock22@gmail.COM>
2023-11-10 14:46:15 +0000

    add form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,33 +1,47 @@
-import React from 'react'
-import { useState, useRef } from 'react'
-import { motion } from 'framer-motion'
-import emailjs from "@emailjs/browser"
-import { styles } from '../styles'
-import { EarthCanvas } from './canvas'
-import { SectionWrapper } from '../hoc'
-import { slideIn } from '../utils/motion'
-
+import React from "react";
+import { useState, useRef } from "react";
+import { motion } from "framer-motion";
+import emailjs from "@emailjs/browser";
+import { styles } from "../styles";
+import { EarthCanvas } from "./canvas";
+import { SectionWrapper } from "../hoc";
+import { slideIn } from "../utils/motion";
 
 const Contact = () => {
-const formRef = useRef()
+  const formRef = useRef();
 
-const [form, setForm] = useState({
-  name: "",
-  email: "",
-  message: ""
-})
-const [loading, setLoading] = useState(false)
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+  const [loading, setLoading] = useState(false);
 
-const handleChange = (e) => {}
+  const handleChange = (e) => {};
 
-const handleSubmit = (e) => {}
-
+  const handleSubmit = (e) => {};
 
   return (
-    <div className='xl:mt-12 xl:flex-row flex-xol-reverse flex gap-10 overflow-hidden'>
-    
-    </div>
-  )
-}
+    <div className="xl:mt-12 xl:flex-row flex-xol-reverse flex gap-10 overflow-hidden">
+      <motion.div
+        variants={slideIn("left", "tween", 0.2, 1)}
+        className="flex-[0.75] bg-black-100 p-8 rounded-2xl"
+      >
+        <p className={styles.sectionSubText}>Get in touch</p>
+        <h3 className={styles.sectionHeadText}>Contact</h3>
 
-export default SectionWrapper(Contact, "contact")
+        <form
+          ref={formRef}
+          onSubmit={handleSubmit}
+          className="mt-12 flex-col gap-8"
+        >
+          <label className="flex flex-col">
+            <span className="text-white font-medium mb-4">Jethro Block</span>
+          </label>
+        </form>
+      </motion.div>
+    </div>
+  );
+};
+
+export default SectionWrapper(Contact, "contact");
