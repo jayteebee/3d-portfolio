1410956bea3b11a7309db2bc16a11404f9c1a9ca
Jethro_Block <jethroblock22@gmail.COM>
2023-11-20 11:53:43 +0000

    sidebar complete

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,13 +1,36 @@
-import React from 'react'
 import {contactMe} from '../constants'
 
 
 const SideBar = () => {
   return (
-    <div>
+    <div style={{
+
+        alignItems: 'center',
+        zIndex:"500",
+        position: "fixed",
+        marginTop: "50vh",
+        marginLeft: "5px"
+    }}>
         {contactMe.map((item, index) => (
-            <div key={`contact-${index}`}>
-                
+            <div 
+            key={`contact-${index}`}
+            onClick={() => window.open(item.link, "_blank")}
+            className="cursor-pointer"
+            style={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "space-between",
+                marginTop: "10px",
+                height: "40px",
+                width: "40px",
+                backgroundColor: "#1d1836",
+                borderRadius: "30%",
+            }}
+            >
+                <img
+                src={item.icon}
+                alt={item.name}
+                />
             </div>
         ))}
     </div>
