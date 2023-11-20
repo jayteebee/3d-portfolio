c39e8ad488dfedefcb7056793c6e86e7c40ea1a5
Jethro_Block <jethroblock22@gmail.COM>
2023-11-20 11:53:33 +0000

    sidebar complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { BrowserRouter } from "react-router-dom"
-import {About, Contact, Experience, Hero, Navbar, Tech, Works, StarsCanvas} from "./components"
+import {About, Contact, Experience, Hero, Navbar, Tech, Works, StarsCanvas, SideBar} from "./components"
 
 const App = () => {
 
   return (
 <BrowserRouter>
     <div className="relative z-0 bg-primary">
+    <SideBar />
       <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
         <Navbar />
         <Hero />
