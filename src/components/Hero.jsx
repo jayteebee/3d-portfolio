8ddfcc9ff12a72750b5ffc876460bf8022a0738d
Jethro_Block <jethroblock22@gmail.COM>
2024-01-14 17:28:51 +0000

    Update Hero component and tailwind.config.cjs

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -39,10 +39,13 @@ const Hero = () => {
 
       </div>
   {/**replace this computer canvas with the video 
-      {!isMobile && <ComputersCanvas />} */}
+      {!isMobile && <ComputersCanvas />} 
+*/}
+
       <div className='absolute xs:bottom-15 bottom-32 w-full flex justify-center items-center'>
       <iframe width="644" height="362" src="https://www.youtube.com/embed/oseSfjrlE_Q?si=jj6RyHJF2x7BDB1k" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowFullScreen></iframe>
-</div>
+</div> 
+
       <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
       <a href='#about'>
       <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
