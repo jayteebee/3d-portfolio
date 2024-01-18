e51e4c87becbc464cfec4b0532a273f5871224b9
Jethro_Block <jethroblock22@gmail.COM>
2024-01-18 18:09:34 +0000

    Refactor Hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,9 @@
-import React, {useState, useEffect} from 'react'
-import {motion} from 'framer-motion';
-import {styles} from "../styles"
-import {ComputersCanvas} from "./canvas"
-import ReactPlayer from 'react-player/youtube'
-import Intro from "../assets/Introduction.mp4"
-import "../../src/hero.css"
+import React, { useState, useEffect } from "react";
+import { motion } from "framer-motion";
+import { styles } from "../styles";
+import { ComputersCanvas } from "./canvas";
+import ReactPlayer from "react-player/youtube";
+// import "../../src/hero.css"
 
 const Hero = () => {
   const [isMobile, setIsMobile] = useState(false);
@@ -26,51 +25,55 @@ const Hero = () => {
   }, []);
 
   return (
-    <section className='relative w-full h-screen mx-auto'>
-    <div className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}>
-      <div className='flex flex-col justify-center items-center mt-5'>
-        <div className='w-5 h-5 rounded-full bg-[#915eff]'/>
-        <div className='w-1 sm:h-80 h-40 violet-gradient'/>
-      </div>
+    <section className="relative w-full h-screen mx-auto " style={{display: "flex", alignContent: "space-between"}}>
+      <div
+        className={`${styles.paddingX} absolute inset-0 top-[120px] max-w-7xl mx-auto flex flex-row items-start gap-5`}
+      >
+        <div className="flex flex-col justify-center items-center mt-5">
+          <div className="w-5 h-5 rounded-full bg-[#915eff]" />
+          <div className="w-1 sm:h-80 h-40 violet-gradient" />
+        </div>
 
-      <div>
-      <h1 className={`${styles.heroHeadText} text-white`}>Hi, I'm <span className='text-[#915eff]'>Jethro</span></h1>
-      <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-      A Software Developer
-      </p>
-      </div>
-
-      </div>
-
-{/**       <div style={{position: "relative", paddingTop: "56.25%", }}>
-<ReactPlayer width="100%" height="100%" style={{position: "absolute", top: 0, left: 0}} url="https://www.youtube.com/embed/oseSfjrlE_Q?si=jj6RyHJF2x7BDB1k"/>
-</div> 
-*/}
-
-  <div className='mt-16 shadow-card absolute xs:bottom-15 bottom-32 w-full flex justify-center items-center hero-video' >
-  <ReactPlayer className=' bg-tertiary green-pink-gradient p-[1px] ' style={{ aspectRatio: "16/9", position: "absolute", bottom: "20%"}} url="https://www.youtube.com/embed/oseSfjrlE_Q?si=jj6RyHJF2x7BDB1k"/>
-  </div> 
-
-
-
-
-      <div className='absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center'>
-      <a href='#about'>
-      <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
-        <motion.div 
-        animate={{y: [0,24,0]}}
-        transition={{
-          duration: 1.5,
-          repeat: Infinity,
-          repeatType: 'loop'
-        }}
-        className="w-3 h-3 rounded-full bg-secondary mb-1k"
-        />
-      </div>
-      </a>
+        <div>
+          <h1 className={`${styles.heroHeadText} text-white`}>
+            Hi, I'm <span className="text-[#915eff]">Jethro</span>
+          </h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+            A Software Developer
+          </p>
+        </div>
+        </div>
+        
+        {/**  
+      <div style={{position: "relative", paddingTop: "56.25%", }}>
+      <ReactPlayer width="100%" height="100%" style={{position: "absolute", top: 0, left: 0}} url="https://www.youtube.com/embed/oseSfjrlE_Q?si=jj6RyHJF2x7BDB1k"/>
+      </div> 
+    */}
+      
+                  <div className="mt-16 shadow-card absolute xs:bottom-15 bottom-32 w-full flex justify-center items-center hero-video" style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+                  <ReactPlayer
+                  className=" bg-tertiary green-pink-gradient p-[1px] flex "
+                  url="https://www.youtube.com/embed/oseSfjrlE_Q?si=jj6RyHJF2x7BDB1k"
+                  />
+                  </div>
+                  
+      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
+        <a href="#about">
+          <div className="w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2">
+            <motion.div
+              animate={{ y: [0, 24, 0] }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: "loop",
+              }}
+              className="w-3 h-3 rounded-full bg-secondary mb-1k"
+            />
+          </div>
+        </a>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
