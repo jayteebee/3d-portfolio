3474a352757d89cceed5b818992d308656317d8e
Jethro_Block <jethroblock22@gmail.COM>
2024-01-22 14:56:06 +0000

    Remove unnecessary code in Hero component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -47,15 +47,9 @@ const Hero = () => {
           url="https://www.youtube.com/embed/oseSfjrlE_Q?si=jj6RyHJF2x7BDB1k"
           />
           </div>
-          
           </div>
         </div>
-        
-        {/**  
-      <div style={{position: "relative", paddingTop: "56.25%", }}>
-      <ReactPlayer width="100%" height="100%" style={{position: "absolute", top: 0, left: 0}} url="https://www.youtube.com/embed/oseSfjrlE_Q?si=jj6RyHJF2x7BDB1k"/>
-      </div> 
-    */}
+
       
                   
       <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
